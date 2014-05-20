946e312ce2bcb0d43ca8cb36ae31e1b3879c401c
David Ormsbee <dave@edx.org>
2014-05-20 15:22:56 -0400

    Early pass at mobile user info API

--- common/djangoapps/student/models.py
+++ common/djangoapps/student/models.py
@@ -37,6 +37,7 @@ from eventtracking import tracker
 from importlib import import_module
 
 from xmodule.modulestore import Location
+from xmodule.modulestore.django import modulestore
 
 from course_modes.models import CourseMode
 import lms.lib.comment_client as cc
@@ -569,7 +570,7 @@ class CourseEnrollment(models.Model):
     """
     MODEL_TAGS = ['course_id', 'is_active', 'mode']
 
-    user = models.ForeignKey(User)
+    user = models.ForeignKey(User, related_name="course_enrollments")
     course_id = models.CharField(max_length=255, db_index=True)
     created = models.DateTimeField(auto_now_add=True, null=True, db_index=True)
 
@@ -937,6 +938,14 @@ class CourseEnrollment(models.Model):
             return False
         else:
             return True
+
+    @property
+    def username(self):
+        return self.user.username
+
+    @property
+    def course(self):
+        return modulestore().get_course(self.course_id)
 
 
 class CourseEnrollmentAllowed(models.Model):
